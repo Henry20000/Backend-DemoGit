d4d19340f88993e91ae756cb9cf3930f79191fe3
Henry20000 <dangducquang999@gmail.com>
2024-08-27 21:33:32 +0700

    Connect DB

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const mongoose = require("mongoose");
 dotenv.config()
 
 const app = express()
@@ -8,6 +9,14 @@ const port = process.env.PORT || 3001
 app.get('/', (req, res) => {
      res.send('Hello World Quang' )
 })
+
+mongoose.connect(`mongodb+srv://gymiseasy:${process.env.MONGO_DB}@cluster0.zmwfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {
+        console.log('Connect Db success!')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 app.listen(port, () => {
     console.log('Server is running in port: ', + port)
